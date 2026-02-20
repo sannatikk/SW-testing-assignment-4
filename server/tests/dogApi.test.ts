bedeb5e62e402a5f30aa75157d09e896799972a3
Sanna Tikkakoski <153513791+sannatikk@users.noreply.github.com>
2026-02-20 19:34:28 +0200

    api test passes

---
+++ server/tests/dogApi.test.ts
@@ -0,0 +1,39 @@
+import { describe, expect, test, vi, beforeEach, afterEach } from 'vitest'
+import request from 'supertest'
+import { app } from '../index'
+import * as dogController from '../controllers/dogController'
+import type { Request, Response } from 'express'
+
+vi.mock('../controllers/dogController')
+
+describe('Dog API tests', () => {
+
+    beforeEach(() => {
+        vi.clearAllMocks()
+    })
+    afterEach(() => {
+        vi.resetAllMocks()
+    })
+
+    test('GET /api/dogs/random returns 500 and error JSON when controller fails', async () => {
+
+    const mockedErrorJson = {
+        success: false,
+        error: 'Failed to fetch dog image: Network error',
+    }
+
+    vi.mocked(dogController.getDogImage).mockImplementation(
+        async (_req: Request, res: Response) => {
+            res.status(500).json(mockedErrorJson)
+        }
+    )
+
+    const response = await request(app).get('/api/dogs/random')
+
+    expect(response.status).toBe(500)
+    expect(response.body.success).toBe(false)
+    expect(response.body.error).toBeDefined()
+    expect(response.body.error).toBe('Failed to fetch dog image: Network error')
+    
+    })
+})
